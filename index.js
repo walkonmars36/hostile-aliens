71d6818091503866bbb4895e9fa6301d1459720a
Mark Lawson <mark@walkonmars.co.uk>
2022-01-20 16:01:09 +0000

    game rendering

--- index.js
+++ index.js
@@ -1,20 +1,21 @@
 // create a class for mothership
-class motherShip {
+class MotherShip {
   constructor(name, type, hitPoints, losePoints) {
     this.name = name;
     this.type = type;
     this.hitPoints = hitPoints;
     this.losePoints = losePoints;
+    this.shipIsDestroyed = false;
   }
 
   // method to display ship
   displayShip() {
-    // something here
+    document.getElementById(this.type).innerHTML += `<p id="${this.name}" > ${this.name} : ${this.hitPoints}</p>`;
   }
 
   // method for losing points when hit
   reducePoints() {
-    this.hitPoints -= losePoints;
+    // this.hitPoints -= losePoints;
   }
 
   //method to update the points display
@@ -22,9 +23,12 @@ class motherShip {
     //   something here
   }
 
-  // method to check if the ship is still alive
-  isShipAlive() {
+  // method to check if the ship is destroyed
+  checkShipIsDestroyed() {
     // something here
+    if (this.hitPoints <= 0) {
+      this.shipIsDestroyed = true;
+    }
   }
 
   // method to remove from view if ship is dead
@@ -34,13 +38,13 @@ class motherShip {
 }
 
 // extend mothership class to create classes for defence ship and attack ship
-class defenceShip extends motherShip {
+class DefenceShip extends MotherShip {
   constructor(name, type, hitPoints, losePoints) {
     super(name, type, hitPoints, losePoints);
   }
 }
 
-class attackShip extends motherShip {
+class AttackShip extends MotherShip {
   constructor(name, type, hitPoints, losePoints) {
     super(name, type, hitPoints, losePoints);
   }
@@ -49,12 +53,41 @@ class attackShip extends motherShip {
 // need ship array to push ships into
 let shipArray = [];
 
-// function to create ships
-const createShips = () => {
-  // something here
+// function to create defence and attack ships, creating a new instance of respective classes and pushing into shipArray
+const createShips = (shipType, numShips) => {
+  for (let i = 0; i < numShips; i++) {
+    if (shipType === "defence") {
+      shipArray.push(new DefenceShip("D" + i, "defence", 80, 10));
+    } else {
+      shipArray.push(new AttackShip("A" + i, "attack", 45, 12));
+    }
+  }
+};
+
+// function to create new game, invoking createShips to build defence and attack, plus a new MotherShip
+const newGame = () => {
+  shipArray = [];
+  shipArray.push(new MotherShip("M", "mothership", 100, 9));
+  createShips("defence", 5);
+  createShips("attack", 8);
+  shipArray.forEach((ship) => ship.displayShip());
 };
 
 // function to fire at random ship
 const hitRandomShip = () => {
   Math.floor(Math.random() * shipArray.length);
 };
+
+// function to check if game is over
+const checkIfGameOver = (shipArray) => {
+  // is mothership destroyed?
+  if (shipArray[0].shipIsDestroyed) {
+    return true;
+  } // check if all ships destroyed
+  if (shipArray.every((ship) => ship.shipIsDestroyed)) {
+    return true;
+  }
+};
+
+// add event listener to play game button to trigger new game function
+document.querySelector(".play-btn").addEventListener("click", () => newGame());
