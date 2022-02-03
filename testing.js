75812685a4166ced6affa420a5ff013eb105b6ad
Mark Lawson <mark@walkonmars.co.uk>
2022-02-03 15:19:58 +0000

    created new js files for testing

--- testing.js
+++ testing.js
@@ -2,7 +2,7 @@
 // This has made my brain hurt, but I've had a lot of breakthroughs and several punch the air moments.
 
 // define class for AlienGame
-class AlienGame {
+export class AlienGame {
   constructor(numOfMotherShips, numOfDefenceShips, numOfAttackShips) {
     this.numOfMotherShips = numOfMotherShips;
     this.numOfDefenceShips = numOfDefenceShips;
@@ -14,9 +14,6 @@ class AlienGame {
 
     this.shipsArray = [];
     this.gameOver = false;
-    this.gameOverMessage = document.querySelector(".game-over");
-    this.fireBtn = document.querySelector(".fire-btn");
-    this.playBtn = document.querySelector(".play-btn");
   }
 
   // method to push all ships into the shipsArray
@@ -36,10 +33,6 @@ class AlienGame {
 
   // method to render shipsArray to the screen
   renderShips() {
-    this.gameOverMessage.style.display = "none";
-    this.fireBtn.style.visibility = "visible";
-    this.playBtn.style.display = "none";
-
     let html = "";
     // loop through array and create div for each ship, push into html
     for (let i = 0; i < this.shipsArray.length; i++) {
@@ -61,26 +54,23 @@ class AlienGame {
   // method to fire at ships, remove points when hit
   fireAndUpdate() {
     // add event listener to the fire-btn
-    document.querySelector(".fire-btn").addEventListener("click", () => {
-      // variable to store the random ship
-      let randomShip = this.getRandomShip();
-      // remove damage points [2] from hitpoints [1] - in each ship's subarray
-      this.shipsArray[randomShip][1] -= this.shipsArray[randomShip][2];
 
-      // Update game - remove ship from shipsArray when hitpoints are <= 0, remove from HTML and check if the game is over
-      if (this.shipsArray[randomShip][1] <= 0) {
-        this.shipsArray.splice(randomShip, 1);
-        this.isGameOver();
-        document.querySelector(".game-container").innerHTML = this.renderShips();
-      }
-      if (this.gameOver === true) {
-        this.endGame();
-      } else {
-        document.querySelector(".game-container").innerHTML = this.renderShips();
-      }
+    // variable to store the random ship
+    let randomShip = this.getRandomShip();
+    // remove damage points [2] from hitpoints [1] - in each ship's subarray
+    this.shipsArray[randomShip][1] -= this.shipsArray[randomShip][2];
 
-      console.log(randomShip);
-    });
+    // Update game - remove ship from shipsArray when hitpoints are <= 0, remove from HTML and check if the game is over
+    if (this.shipsArray[randomShip][1] <= 0) {
+      this.shipsArray.splice(randomShip, 1);
+      this.isGameOver();
+      this.renderShips();
+    }
+    if (this.gameOver === true) {
+      this.endGame();
+    } else {
+      this.renderShips();
+    }
   }
 
   isGameOver() {
@@ -102,22 +92,11 @@ class AlienGame {
   }
 
   resetGame() {
-    document.querySelector(".play-btn").addEventListener("click", () => {
-      this.gameOver = false;
-      this.shipsArray = [];
+    this.gameOver = false;
+    this.shipsArray = [];
 
-      this.createShips();
+    this.createShips();
 
-      document.querySelector(".game-container").innerHTML = this.renderShips();
-    });
+    this.renderShips();
   }
 }
-
-// create new instance of AlienGame,
-const playGame = new AlienGame(1, 5, 8);
-// with arguments for - numOfMotherShips, numOfAttackShips, num of DefenceShips
-playGame.createShips();
-document.querySelector(".game-container").innerHTML = playGame.renderShips();
-
-playGame.fireAndUpdate();
-playGame.resetGame();
--- testing.test.js
+++ testing.test.js
@@ -1,21 +1,21 @@
 import {expect, it, test} from "@jest/globals";
-import {AlienGame} from "./index.js";
+import {AlienGame} from "./testing.js";
 
 it("Should create correct number in array", () => {
-  const play = new AlienGame();
-  play.createShips(1, 5, 8);
+  const play = new AlienGame(1, 5, 8);
+  play.createShips();
   expect(play.shipsArray.length).toEqual(14);
 });
 
 it("should check hit points and damage points of ships are correct", () => {
-  const play = new AlienGame();
+  const play = new AlienGame(1, 5, 8);
   expect(play.motherShip.slice(1, 3)).toEqual([100, 9]);
   expect(play.defenceShip.slice(1, 3)).toEqual([80, 10]);
   expect(play.attackShip.slice(1, 3)).toEqual([45, 12]);
 });
 
 it("should select a random integer between 0 and the length of ships array", () => {
-  const play = new AlienGame();
+  const play = new AlienGame(1, 5, 8);
   expect(play.getRandomShip()).toBeGreaterThanOrEqual(0);
   expect(play.getRandomShip()).toBeLessThanOrEqual(play.shipsArray.length);
 });
