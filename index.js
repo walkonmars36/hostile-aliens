6356e1ff8d308f3968074780d9e05ee0cf853290
Mark Lawson <mark@walkonmars.co.uk>
2022-02-03 11:22:15 +0000

    game working, a few tests to complete

--- index.js
+++ index.js
@@ -1,39 +1,123 @@
-// Second attempt, after much study I'll attempt to do all in OOP with no additional functions and test from the outset using jest
+// Second attempt, after much study and pain I'll attempt to do all in OOP with no additional functions and test from the outset using jest.
+// This has made my brain hurt, but I've had a lot of breakthroughs and several punch the air moments.
 
 // define class for AlienGame
-
-export class AlienGame {
+class AlienGame {
   constructor(numOfMotherShips, numOfDefenceShips, numOfAttackShips) {
     this.numOfMotherShips = numOfMotherShips;
     this.numOfDefenceShips = numOfDefenceShips;
     this.numOfAttackShips = numOfAttackShips;
 
-    this.motherShip = ["Mother Ship", 100, 9, this.numOfMotherShips];
-    this.defenceShip = ["Defence Ship", 80, 10, this.numOfDefenceShips];
-    this.attackShip = ["Attack Ship", 45, 12, this.numOfAttackShips];
+    this.motherShip = ["Mother-Ship", 100, 9];
+    this.defenceShip = ["Defence-Ship", 80, 10];
+    this.attackShip = ["Attack-Ship", 45, 12];
 
     this.shipsArray = [];
     this.gameOver = false;
+    this.gameOverMessage = document.querySelector(".game-over");
+    this.fireBtn = document.querySelector(".fire-btn");
+    this.playBtn = document.querySelector(".play-btn");
   }
 
   // method to push all ships into the shipsArray
   createShips() {
     this.shipsArray.push([this.motherShip[0], this.motherShip[1], this.motherShip[2]]);
 
-    for (let i = 0; i < arguments[1]; i++) {
+    for (let i = 0; i < this.numOfDefenceShips; i++) {
       this.shipsArray.push([this.defenceShip[0], this.defenceShip[1], this.defenceShip[2]]);
     }
 
-    for (let i = 0; i < arguments[2]; i++) {
+    for (let i = 0; i < this.numOfAttackShips; i++) {
       this.shipsArray.push([this.attackShip[0], this.attackShip[1], this.attackShip[2]]);
     }
 
     return this.shipsArray;
   }
+
+  // method to render shipsArray to the screen
+  renderShips() {
+    this.gameOverMessage.style.display = "none";
+    this.fireBtn.style.visibility = "visible";
+    this.playBtn.style.display = "none";
+
+    let html = "";
+    // loop through array and create div for each ship, push into html
+    for (let i = 0; i < this.shipsArray.length; i++) {
+      html += `<div class='${this.shipsArray[i][0].toLowerCase()}'>`;
+      for (let j = 0; j < 2; j++) {
+        html += `${this.shipsArray[i][j]} `;
+      }
+
+      html += "</div>";
+    }
+    return html;
+  }
+
+  // method to get a random ship to fire at
+  getRandomShip() {
+    return Math.floor(Math.random() * this.shipsArray.length);
+  }
+
+  // method to fire at ships, remove points when hit
+  fireAndUpdate() {
+    // add event listener to the fire-btn
+    document.querySelector(".fire-btn").addEventListener("click", () => {
+      // variable to store the random ship
+      let randomShip = this.getRandomShip();
+      // remove damage points [2] from hitpoints [1] - in each ship's subarray
+      this.shipsArray[randomShip][1] -= this.shipsArray[randomShip][2];
+
+      // Update game - remove ship from shipsArray when hitpoints are <= 0, remove from HTML and check if the game is over
+      if (this.shipsArray[randomShip][1] <= 0) {
+        this.shipsArray.splice(randomShip, 1);
+        this.isGameOver();
+        document.querySelector(".game-container").innerHTML = this.renderShips();
+      }
+      if (this.gameOver === true) {
+        this.endGame();
+      } else {
+        document.querySelector(".game-container").innerHTML = this.renderShips();
+      }
+
+      console.log(randomShip);
+    });
+  }
+
+  isGameOver() {
+    const containsMother = this.shipsArray.find((ship) => ship[0] === "Mother-Ship");
+
+    if (!containsMother) {
+      this.gameOver = true;
+      this.endGame();
+    } else {
+      return;
+    }
+  }
+
+  endGame() {
+    this.shipsArray = [];
+    this.gameOverMessage.style.display = "block";
+    this.fireBtn.style.visibility = "hidden";
+    this.playBtn.style.display = "block";
+  }
+
+  resetGame() {
+    document.querySelector(".play-btn").addEventListener("click", () => {
+      this.gameOver = false;
+      this.shipsArray = [];
+
+      this.createShips();
+      // this.fireAndUpdate();
+      document.querySelector(".game-container").innerHTML = this.renderShips();
+    });
+  }
 }
 
-// create new instance of AlienGame, with arguments for parameters - numOfMotherShips, numOfAttackShips, num of DefenceShips
-const playGame = new AlienGame();
+// create new instance of AlienGame,
+const playGame = new AlienGame(1, 5, 8);
+// with arguments for - numOfMotherShips, numOfAttackShips, num of DefenceShips
+playGame.createShips();
+document.querySelector(".game-container").innerHTML = playGame.renderShips();
 
-playGame.createShips(1, 5, 8);
-console.log(playGame.shipsArray);
+playGame.fireAndUpdate();
+playGame.resetGame();
--- index.test.js
+++ index.test.js
@@ -1,8 +1,21 @@
-import {it} from "@jest/globals";
+import {expect, it, test} from "@jest/globals";
 import {AlienGame} from "./index.js";
 
-it("correct number in array", () => {
+it("Should create correct number in array", () => {
   const play = new AlienGame();
   play.createShips(1, 5, 8);
   expect(play.shipsArray.length).toEqual(14);
+});
+
+it("should check hit points and damage points of ships are correct", () => {
+  const play = new AlienGame();
+  expect(play.motherShip.slice(1, 3)).toEqual([100, 9]);
+  expect(play.defenceShip.slice(1, 3)).toEqual([80, 10]);
+  expect(play.attackShip.slice(1, 3)).toEqual([45, 12]);
+});
+
+it("should select a random integer between 0 and the length of ships array", () => {
+  const play = new AlienGame();
+  expect(play.getRandomShip()).toBeGreaterThanOrEqual(0);
+  expect(play.getRandomShip()).toBeLessThanOrEqual(play.shipsArray.length);
 });
